fb75fa9fab178c7d90aca54dfb2b25c9cf75379a
Jinboi <duddnwkfgo7@gmail.com>
2024-12-21 17:13:32 -0800

    added proj

---
+++ AnimePortraitDudeASPReact/AnimePortraitDudeASPReact/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿// Controllers/ProductsController.cs
+using Microsoft.AspNetCore.Mvc;
+using YourNamespace.Data;
+using YourNamespace.Models;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProducts), new { id = product.Id }, product);
+        }
+    }
+}
